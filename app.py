f8484c68cf5bd58dd0422e8d9c10b1cd4004b263
Hens743 <80003512+Hens743@users.noreply.github.com>
2024-07-04 10:47:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def recommend_sql_type(col_name, series):
     else:
         return "TEXT"
 
-st.title("Dataset analyzer / SQL data type recommender")
+st.title("Python dataset analyzer / SQL data type recommender")
 
 st.write("""
 ## Instructions
